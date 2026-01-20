9b343e90ac22e36fcccbefee5b36db05886a540c
Z3did <lukas@lprasse.de>
2026-01-20 10:50:12 +0100

    V1

---
+++ src/js/bs-pdp.js
@@ -0,0 +1,155 @@
+  function ThumbnailPlugin(main) {
+  return (slider) => {
+    function removeActive() {
+      slider.slides.forEach((slide) => {
+        slide.classList.remove("active")
+      })
+    }
+    function addActive(index) {
+      slider.slides[index].classList.add("active")
+    }
+
+    function addClickEvents() {
+      slider.slides.forEach((slide, index) => {
+        slide.addEventListener("click", () => {
+          main.moveToIdx(index)
+        })
+      })
+    }
+
+    slider.on("created", () => {
+      addActive(slider.track.details.rel)
+      addClickEvents()
+      main.on("animationStarted", (main) => {
+        removeActive()
+        const next = main.animator.targetIdx || 0
+        addActive(main.track.absToRel(next))
+        slider.moveToIdx(Math.min(slider.track.details.maxIdx, next))
+      })
+    })
+  }
+}
+
+function navigation(slider) {
+  let wrapper, dots, arrowLeft, arrowRight
+
+  function markup(remove) {
+    wrapperMarkup(remove)
+    dotMarkup(remove)
+    arrowMarkup(remove)
+  }
+
+  function removeElement(elment) {
+    elment.parentNode.removeChild(elment)
+  }
+  function createDiv(className) {
+    var div = document.createElement("div")
+    var classNames = className.split(" ")
+    classNames.forEach((name) => div.classList.add(name))
+    return div
+  }
+
+  function arrowMarkup(remove) {
+    if (remove) {
+      removeElement(arrowLeft)
+      removeElement(arrowRight)
+      return
+    }
+    arrowLeft = createDiv("arrow arrow--left")
+    arrowLeft.addEventListener("click", () => slider.prev())
+    arrowRight = createDiv("arrow arrow--right")
+    arrowRight.addEventListener("click", () => slider.next())
+
+    wrapper.appendChild(arrowLeft)
+    wrapper.appendChild(arrowRight)
+  }
+
+  function wrapperMarkup(remove) {
+    if (remove) {
+      var parent = wrapper.parentNode
+      while (wrapper.firstChild)
+        parent.insertBefore(wrapper.firstChild, wrapper)
+      removeElement(wrapper)
+      return
+    }
+    wrapper = createDiv("navigation-wrapper")
+    slider.container.parentNode.appendChild(wrapper)
+    wrapper.appendChild(slider.container)
+  }
+
+  function dotMarkup(remove) {
+    if (remove) {
+      removeElement(dots)
+      return
+    }
+    dots = createDiv("dots")
+    slider.track.details.slides.forEach((_e, index) => {
+      var dot = createDiv("dot")
+      dot.addEventListener("click", () => slider.moveToIdx(index))
+      dots.appendChild(dot)
+    })
+    wrapper.appendChild(dots)
+  }
+
+  function updateClasses() {
+    var slide = slider.track.details.rel
+    slide === 0
+      ? arrowLeft.classList.add("arrow--disabled")
+      : arrowLeft.classList.remove("arrow--disabled")
+    slide === slider.track.details.slides.length - 1
+      ? arrowRight.classList.add("arrow--disabled")
+      : arrowRight.classList.remove("arrow--disabled")
+    Array.from(dots.children).forEach(function (dot, index) {
+      index === slide
+        ? dot.classList.add("dot--active")
+        : dot.classList.remove("dot--active")
+    })
+  }
+
+  slider.on("created", () => {
+    markup()
+    updateClasses()
+  })
+  slider.on("optionsChanged", () => {
+    console.log(2)
+    markup(true)
+    markup()
+    updateClasses()
+  })
+  slider.on("slideChanged", () => {
+    updateClasses()
+  })
+  slider.on("destroyed", () => {
+    markup(true)
+  })
+}
+
+function setThumbnailHeight( timeout = false ) {
+  const setHeight = () => document.querySelector( '#thumbnails').style.height = `${ document.querySelector('#main-gallery img').clientHeight - 40 }px`
+  if ( timeout ) return setTimeout( () => {
+    document.querySelector( '#thumbnails').style.height = `${ document.querySelector('#main-gallery img').clientHeight - 40 }px`
+  })
+  setHeight()
+}
+
+var slider = new KeenSlider("#main-gallery", {}, [navigation])
+
+window.onload = () => {
+  setThumbnailHeight()
+  var thumbnails = new KeenSlider(
+    "#thumbnails",
+    {
+      initial: 0,
+      slides: {
+        perView: 4,
+        spacing: 10,
+      },
+      vertical: true
+    },
+    [ThumbnailPlugin(this.slider)]
+  )
+
+  window.addEventListener( 'resize', () => setThumbnailHeight( true ), { passive: true } )
+}
+
+      
